244fd21d08addbdba9ab01e23efcf1293c494582
unknown <abood.jallad.2001@gmail.com>
2022-03-09 22:07:45 +0200

    Add first magic number checker

--- Checkers.py
+++ Checkers.py
@@ -1,4 +1,22 @@
 import FileReader
+import re
 
-print(FileReader.content)
+masseges = []
+linenumber = 1
+
+
+def magicNumberAfterOperator(masseges, linenumber):
+
+    result = []
+    operators = ['+', '-', '*', '/', '%']
+    for line in FileReader.lines:
+        for operator in operators:
+            if operator in line:
+                result = re.findall(".*[0-9]", line)
+                result += ['contain magic number', 'in line', linenumber]
+                masseges += [result]
+        linenumber = linenumber + 1
+
+
+
 
